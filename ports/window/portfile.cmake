b26538289d878b5eaa4cc9c3d14cfd1fd92aac7f
Ithier Débauché <ithier.debauche@etu.univ-lyon1.fr>
2025-07-14 22:05:50 +0200

    update window lib

--- ports/window/portfile.cmake
+++ ports/window/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Elnhomantia/window
-    REF v1.0.0
+    REF v1.0.1
     HEAD_REF release
-    SHA512 050442f504fc93e4620d738c6f103e2c423e439f28eba6348b864a6e2e8ac750aa39f1500cbb3b0582f3c13964a4b00be9dc00cfc1565e2f8380ca16a58dc68a
+    SHA512 3a4b2d57f30373fdb48e2d940c13934e4cc82f34aa1c620e52aada93c676f113434703956d3dfb27fe4e9f7a46fb4731fd0de0d330a834083229cbfdbc51b429
 )
 
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/window)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
