55eedf47f1b643b783683ecd47149ddd37852d5e
Ithier Débauché <ithier.debauche@etu.univ-lyon1.fr>
2025-07-14 22:23:07 +0200

    update window

--- ports/window/portfile.cmake
+++ ports/window/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Elnhomantia/window
-    REF v1.0.1
+    REF v1.0.2
     HEAD_REF release
-    SHA512 3a4b2d57f30373fdb48e2d940c13934e4cc82f34aa1c620e52aada93c676f113434703956d3dfb27fe4e9f7a46fb4731fd0de0d330a834083229cbfdbc51b429
+    SHA512 950db64357be31c6fee77324d8158eafdd49cf7040c162d8577dda42b1784ccdab3bf6979b9e23e0a3aa448765f1aa7c56f2a0a6a71be72f387f3e03ea166524
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
