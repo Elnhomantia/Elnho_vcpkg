eead03891dd1d0d64c0cd97d4431bf655171a946
Ithier Débauché <ithier.debauche@etu.univ-lyon1.fr>
2025-07-14 19:08:15 +0200

    forgot to add sha512

--- ports/window/portfile.cmake
+++ ports/window/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO Elnhomantia/window
     REF v1.0.0
     HEAD_REF release
-    SHA512 
+    SHA512 050442f504fc93e4620d738c6f103e2c423e439f28eba6348b864a6e2e8ac750aa39f1500cbb3b0582f3c13964a4b00be9dc00cfc1565e2f8380ca16a58dc68a
 )
 
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
